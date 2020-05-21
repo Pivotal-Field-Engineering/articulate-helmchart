ab80af12d2b14e76a367244bb3844a896c8a0859
Hemanth AVS <hemanth.avs@gmail.com>
2020-05-20 21:37:50 -0500

    Updated Deployment with Env File

--- articulate/notes/command.sh
+++ articulate/notes/command.sh
@@ -3,8 +3,12 @@ currentSlot=`(helm get values --all test | yq r - productionSlot)`
 if [ "$currentSlot" == "blue" ];
 then
   newSlot="green"
+  echo "Deploying Code to Green Environment"
+  echo "http://articulate-bg-stage.caas.pez.pivotal.io"
 else
   newSlot="blue"
+  echo "Deploying Code to Blue Environment"
+  echo "http://articulate-bg-prod.caas.pez.pivotal.io"
 fi
 
-echo "newSlot=$newSlot \ncurrentSlot=$currentSlot" | pbcopy
+echo "\n newSlot=$newSlot \n currentSlot=$currentSlot \n enableSlot=$newSlot.enabled=true \n productionSlot=productionSlot=$newSlot" | pbcopy
